34cd75dab697042d90912ca23a9d6eb2796bdf18
M. Hille <marciausgoslar@gmail.com>
2019-11-21 11:54:41 +0100

    Added question-form component and quiz logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, QuestionFormComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
