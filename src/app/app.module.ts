1ddcfe07ec7b467f7522a8f0f745402a7765ae7f
M. Hille <marciausgoslar@gmail.com>
2019-11-23 08:04:15 +0100

    Added results component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
-  declarations: [AppComponent, QuestionFormComponent],
+  declarations: [AppComponent, QuestionFormComponent, ResultsComponent],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
